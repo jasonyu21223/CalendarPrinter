3ba79ee10879247e77ef31a4b89f3f857625a83f
jasonyu21223 <jason21223@gmail.com>
2015-09-28 19:24:02 +0000

    ASDAD.

---
+++ CalendarPrinter.java
@@ -0,0 +1,12 @@
+public class CalendarPrinter
+{
+    public static void main (String[] args) {
+        //your code here
+        int x= 4;
+        while ( x<31 ){
+        System.out.println("September 1");
+     x++;
+        }
+  
+    }
+}
